1ccb83a85b11450e3e87bce17cdc78850dd8f789
jdmy <1575529757@qq.com>
2018-10-15 15:12:26 +0800

    init

---
+++ pro-netty/src/main/java/cn/bushadie/pronetty/ProNettyApplication.java
@@ -0,0 +1,12 @@
+package cn.bushadie.pronetty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProNettyApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( ProNettyApplication.class, args );
+    }
+}
