3e5fd8abfe711bf8ab41e2a79951a4b5c0cd25ef
jdmy <1575529757@qq.com>
2018-10-16 17:36:38 +0800

    生命周期

--- pro-netty/src/main/java/cn/bushadie/pronetty/netty/CustomHandler.java
+++ pro-netty/src/main/java/cn/bushadie/pronetty/netty/CustomHandler.java
@@ -38,4 +38,64 @@ public class CustomHandler extends SimpleChannelInboundHandler< HttpObject > {
         }
 
     }
+
+    @Override
+    public void channelRegistered( ChannelHandlerContext ctx ) throws Exception {
+        System.out.println( "channel------注册" );
+        super.channelRegistered( ctx );
+    }
+
+    @Override
+    public void channelUnregistered( ChannelHandlerContext ctx ) throws Exception {
+        System.out.println( "channel------移除" );
+        super.channelUnregistered( ctx );
+    }
+
+    @Override
+    public void channelActive( ChannelHandlerContext ctx ) throws Exception {
+        System.out.println( "channel------活跃" );
+        super.channelActive( ctx );
+    }
+
+    @Override
+    public void channelInactive( ChannelHandlerContext ctx ) throws Exception {
+        System.out.println( "channel------不活跃" );
+        super.channelInactive( ctx );
+    }
+
+    @Override
+    public void channelReadComplete( ChannelHandlerContext ctx ) throws Exception {
+        System.out.println( "channel------读取完毕" );
+        super.channelReadComplete( ctx );
+    }
+
+    @Override
+    public void userEventTriggered( ChannelHandlerContext ctx, Object evt ) throws Exception {
+        System.out.println( "channel------用户事件触发" );
+        super.userEventTriggered( ctx, evt );
+    }
+
+    @Override
+    public void channelWritabilityChanged( ChannelHandlerContext ctx ) throws Exception {
+        System.out.println( "channel------可写更改" );
+        super.channelWritabilityChanged( ctx );
+    }
+
+    @Override
+    public void handlerAdded( ChannelHandlerContext ctx ) throws Exception {
+        System.out.println( "channel------助手类添加" );
+        super.handlerAdded( ctx );
+    }
+
+    @Override
+    public void handlerRemoved( ChannelHandlerContext ctx ) throws Exception {
+        System.out.println( "channel-------助手类移除" );
+        super.handlerRemoved( ctx );
+    }
+
+    @Override
+    public void exceptionCaught( ChannelHandlerContext ctx, Throwable cause ) throws Exception {
+        System.out.println( "channel------捕获到异常" );
+        super.exceptionCaught( ctx, cause );
+    }
 }
